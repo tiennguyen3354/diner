900702c5a4eacf307472738d8a388bfafd803527
Tien Nguyen <tiennguyen3354@gmail.com>
2024-02-27 05:56:55 -0500

    added controller to github

--- controller/controller.php
+++ controller/controller.php
@@ -62,6 +62,8 @@ class Controller
                 $this->_f3->set('SESSION.order', $order);
 //                var_dump($this->_f3->get('SESSION.order'));
                 //if there are no error reroute somewhere NEED TO DO
+                $this->_f3->reroute("order-form-2");
+
             }
         }
 
@@ -70,5 +72,33 @@ class Controller
         $this->_f3->set("meals" , DataLayer::getMeals());
         $view = new Template();
         echo $view->render('views/order-form-1.html');
+    }
+
+    function order2()
+    {
+        //part2 this will check to see if the method is POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //check if the condiments are valid
+            if(isset($_POST['condiment'])) {
+                $condimentString = implode("," , $_POST['condiment']);
+                echo $condimentString;
+            }else{
+                $this->_f3->set('error["condiments]', "None of the condiment is selected. ");
+            }
+
+            //if there are no errors.
+            if(empty($this->_f3->get('error'))){
+                $this->_f3->get('SESSION.order')-> setCondiments($condimentString);
+                $this->_f3->reroute('summary');
+            }
+
+        }
+
+        //part 1 this will display the page if the method is GET
+        //put the data into an array so you can access it at the html page
+        $this->_f3->set("condiments" , DataLayer::getCondiments());
+        // make the page visible
+        $view = new Template();
+        echo $view->render('views/order-form-2.html');
     }
 }
